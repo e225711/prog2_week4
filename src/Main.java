fecb8bf4bf10538bbd97ab605aa80f87f1f78421
Maimu Kohagura <e225711@ie.u-ryukyu.ac.jp>
2022-11-03 13:43:40 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("main method runs");
+    Chatbot bot1 = new Chatbot();
+    Chatbot bot2 = new Chatbot("舞武");
+    bot1.greeting();
+    bot2.greeting();
+    bot2.name = "小波蔵";
+    bot2.greeting();
+    
+
+
+
+  }
+}
